c1286fe913cf7fc145a2f3a81cc8359cd56b49ca
hubgit7482aman <amanchandra7482@gmail.com>
2025-04-18 23:10:40 +0530

    created user profile page

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -14,6 +14,7 @@ router.post("/logout", logout);
 router.put("/update-profile",protectRoute,updateProfile);  
 // ham protect route vala code har component mai likh sakte hai lekin kai route honge jinmai enter karne se pahle user(authenticate hai ya nahi check karna padega), toh separate protectroute bana liye as a middleware
 
+
 router.get("/check",protectRoute, checkAuth);
 
 export default router;
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -62,4 +62,19 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response.data.message);
     }
   },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.error("Update profile error:", error);
+      const message = error?.response?.data?.message || "Update profile failed";
+      toast.error(message);
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
 }));
